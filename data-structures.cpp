98f726cf08c64e69142d8669c4fbde0d88eb9782
SeanPMulligan <seanmulligan@protonmail.com>
2020-07-31 08:32:11 -0700

    Linked List Node implementation

--- data-structures.cpp
+++ data-structures.cpp
@@ -1,8 +1,12 @@
 #include "./stack/stack.cpp"
 #include "./queues/queue.cpp"
+#include "./linked_lists/node.cpp"
 
 int main() {
   // Stack example
+  std::cout << "####################" << std::endl;
+  std::cout << "Stack Implementation"<< std::endl;
+  std::cout << "####################" << std::endl;
   Stack st(10);
 
   st.createEmptyStack();
@@ -21,6 +25,9 @@ int main() {
 
   std::cout<< std::endl;
   // Basic Queue example
+  std::cout << "####################" << std::endl;
+  std::cout << "Queue Implementation"<< std::endl;
+  std::cout << "####################" << std::endl;
 
   Queue q(5);
   q.enqueue(1);
@@ -31,9 +38,6 @@ int main() {
   q.enqueue(6);
   q.enqueue(7);
 
-
-  std::cout<< std::endl;
-  std::cout << "##############" << std::endl;
   std::cout << std::endl;
 
   q.dequeue();
@@ -44,4 +48,30 @@ int main() {
   q.dequeue();
   q.dequeue();
   q.dequeue();
+
+  std::cout<< std::endl;
+  // Linked List node example
+  std::cout << "###############################" << std::endl;
+  std::cout << "Linked List Node Implementation"<< std::endl;
+  std::cout << "###############################" << std::endl;
+
+  Node* head = new Node();
+  Node* two = new Node();
+  Node* three = new Node();
+  Node* four = new Node();
+
+  head->data = 1;
+  head->next = two;
+  two->data = 2;
+  two->next = three;
+  three->data = 3;
+  three->next = four;
+  four->data = 4;
+
+  Node* curr = head;
+
+  while (curr != nullptr) {
+    std::cout << curr->data << std::endl;
+    curr = curr->next;
+  }
 }

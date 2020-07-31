9dc90df9d00613db3386e07f3ae56392223f5d26
SeanPMulligan <seanmulligan@protonmail.com>
2020-07-30 19:37:00 -0700

    Basic Queue implementation

--- data-structures.cpp
+++ data-structures.cpp
@@ -1,4 +1,5 @@
 #include "./stack/stack.cpp"
+#include "./queues/queue.cpp"
 
 int main() {
   // Stack example
@@ -17,4 +18,30 @@ int main() {
   st.pop();
   st.pop();
   st.pop();
+
+  std::cout<< std::endl;
+  // Basic Queue example
+
+  Queue q(5);
+  q.enqueue(1);
+  q.enqueue(2);
+  q.enqueue(3);
+  q.enqueue(4);
+  q.enqueue(5);
+  q.enqueue(6);
+  q.enqueue(7);
+
+
+  std::cout<< std::endl;
+  std::cout << "##############" << std::endl;
+  std::cout << std::endl;
+
+  q.dequeue();
+  q.peek();
+  q.dequeue();
+  q.dequeue();
+  q.dequeue();
+  q.dequeue();
+  q.dequeue();
+  q.dequeue();
 }
